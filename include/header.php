21f89c815a51256afdc45e854ad5affe63fcc6c7
Davans01 <Daniel@maikelvanstraten.nl>
2019-05-21 22:11:16 +0200

    Added a way to make a post and post comments

--- include/header.php
+++ include/header.php
@@ -2,15 +2,9 @@
     <nav>
         <ul>
             <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
-<<<<<<< HEAD
-            <div id="Assortimentbtn"><li><a href="assortiment.php">Een post maken</a></li></div>
+            <div id="Assortimentbtn"><li><a href="post.php">Een post maken</a></li></div>
             <div id="Loginbtn"><li><a href="login.php">Login</a></li></div>
             <div id="Registerbtn"><li><a href="registeren.php">registeren</a></li><div>
-=======
-            <div id="Assortimentbtn"><li><a href="games.php">Games</a></li></div>
-            <div id="Loginbtn"><li><a href="login.php">Login</a></li></div>
-            <div id="Registerbtn"><li><a href="register.php">registeren</a></li><div>
->>>>>>> f72f1aa3198ae9f182d045e92c8c99078e2d0b6e
         </ul>
     </nav>
 </header>
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php
+include("include/config.php");
+
+ ?>
 <!DOCTYPE HTML>
 <html lang="nl">
     <head>
     <!-- Include head.php -->
     <?php include("include/head.php") ?>
-    <link rel="stylesheet" href="css/styles.css">
+    <!-- <link rel="stylesheet" href="css/styles.css"> -->
     </head>
     <body>
         <header>
@@ -11,24 +15,38 @@
         <?php include("include/header.php") ?>
         </header>
 
-    <div class="home">
         <p>Welkom op onze website JoinUp. <br> <br>
            Ben jij een gamer en wil je vrienden maken in de buurt? Dit kan met onze website. <br>
            Op deze website kunt u eigen LAN Party's organiseren, om van de gamers in de buurt, vrienden te maken.
         <p>
-    </div>
 
 
-    <div class="populair">
+
+
         <p>Populaire games: <br>
-           League of legends <br>
-           <img src="image/lol.jpg" alt="League of Legends" width="300px" height="200px"> <br>
-           Counterstrike: Global offensive <br>
-           <img src="image/csgo.jpg" alt="Counterstrike: Global Offence" width="300px" height="200px"> <br>
-           Rainbow Six Siege <br>
-           <img src="image/siege.jpg" alt="Rainbow Six Siege" width="300px" height="200px"> <br>
-    </div>
+           League of legends <img src="image/lol.jpg" alt="League of Legends"> <br>
+           Counterstrike: Global offensive <img src="image/csgo.jpg" alt="Counterstrike: Global Offence"> <br>
+           Rainbow Six Siege <img src="image/siege.jpg" alt="Rainbow Six Siege"> <br>
+           <?php
+           mysqli_select_db($conn, 'join_up');
+           $sql = "SELECT * FROM forum";
+           $forums = mysqli_query($conn, $sql);
 
+           foreach ($forums as $forum) {
+               echo "<pre>";
+               echo "<a href='comment.php?id=".$forum['id']."' >".$forum['name']."</a>";
+               echo "<br>";
+               echo $forum['description'];
+               echo "<br>";
+               echo "<br>";
+               // var_dump($forum);
+               echo "</pre>";
+           }
+           // mysqli_query("SELECT * FROM forum");
+
+           // print_r($result);
+
+           ?>
         <footer>
             <!-- Include footer.php -->
             <?php include("include/footer.php") ?>
