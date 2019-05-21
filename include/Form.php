c6bac5db87e609f95da5f6c9d1d20f29e462ebda
Finley <1040237@mborijnland.nl>
2019-05-21 14:46:08 +0200

    Form.php added

---
+++ include/Form.php
@@ -0,0 +1,5 @@
+<form method="POST">
+    Title: <br><input type="text" name="Title"><br>
+    Description: <br><textarea row="50" col="50" name="Description"></textarea><br>
+    <input type="submit" name="submit">
+</form>
