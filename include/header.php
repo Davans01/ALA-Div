ec51958b3dd9ab78d61fdf4b0aad19538abfc9aa
Davans01 <Daniel@maikelvanstraten.nl>
2019-05-21 14:46:27 +0200

    De nav verandert

--- include/header.php
+++ include/header.php
@@ -2,9 +2,9 @@
     <nav>
         <ul>
             <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
-            <div id="Assortimentbtn"><li><a href="assortiment.php">Assortiment</a></li></div>
+            <div id="Assortimentbtn"><li><a href="assortiment.php">Een post maken</a></li></div>
             <div id="Loginbtn"><li><a href="login.php">Login</a></li></div>
-            <div id="Registerbtn"><li><a href="bestelling.php">registeren</a></li><div>
+            <div id="Registerbtn"><li><a href="registeren.php">registeren</a></li><div>
         </ul>
     </nav>
 </header>
