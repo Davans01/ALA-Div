45b592cfb29bbb04e05162179100d0893c8cafd9
MathijsE <48790027+MathijsE@users.noreply.github.com>
2019-05-21 13:06:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,18 +11,23 @@
         <?php include("include/header.php") ?>
         </header>
 
+    <div class="home">
         <p>Welkom op onze website JoinUp. <br> <br>
            Ben jij een gamer en wil je vrienden maken in de buurt? Dit kan met onze website. <br>
            Op deze website kunt u eigen LAN Party's organiseren, om van de gamers in de buurt, vrienden te maken.
         <p>
+    </div>
 
 
-
-
+    <div class="populair">
         <p>Populaire games: <br>
-           League of legends <img src="image/lol.jpg" alt="League of Legends"> <br>
-           Counterstrike: Global offensive <img src="image/csgo.jpg" alt="Counterstrike: Global Offence"> <br>
-           Rainbow Six Siege <img src="image/siege.jpg" alt="Rainbow Six Siege"> <br>
+           League of legends <br>
+           <img src="image/lol.jpg" alt="League of Legends" width="300px" height="200px"> <br>
+           Counterstrike: Global offensive <br>
+           <img src="image/csgo.jpg" alt="Counterstrike: Global Offence" width="300px" height="200px"> <br>
+           Rainbow Six Siege <br>
+           <img src="image/siege.jpg" alt="Rainbow Six Siege" width="300px" height="200px"> <br>
+    </div>
 
         <footer>
             <!-- Include footer.php -->
