7ddd94dedde9c068789dd047ce01a240f94faa33
MathijsE <48790027+MathijsE@users.noreply.github.com>
2019-05-22 11:01:17 +0200

    div id een post maken aangepast

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
     <nav>
         <ul>
             <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
-            <div id="Assortimentbtn"><li><a href="post.php">Een post maken</a></li></div>
+            <div id="Postbtn"><li><a href="post.php">Een post maken</a></li></div>
         </ul>
     </nav>
 </header>
