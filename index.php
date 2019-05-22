c4d2ce575434b68a91187ada179962e16c9a9d79
MathijsE <48790027+MathijsE@users.noreply.github.com>
2019-05-22 10:40:45 +0200

    index images gefixed

--- index.php
+++ index.php
@@ -15,18 +15,28 @@ include("include/config.php");
         <?php include("include/header.php") ?>
         </header>
 
+    <div class="home">
         <p>Welkom op onze website JoinUp. <br> <br>
            Ben jij een gamer en wil je vrienden maken in de buurt? Dit kan met onze website. <br>
            Op deze website kunt u eigen LAN Party's organiseren, om van de gamers in de buurt, vrienden te maken.
         <p>
+    </div>
 
 
+    <div class="populair">
+        <p>Populaire games: <br><br>
+           League of Legends <br>
+           <img src="image/lol.jpg" alt="League of Legends"> <br>
 
+           Counterstrike: Global Offensive <br>
+           <img src="image/csgo.jpg" alt="Counterstrike: Global Offence"> <br>
 
-        <p>Populaire games: <br>
-           League of legends <img src="image/lol.jpg" alt="League of Legends"> <br>
-           Counterstrike: Global offensive <img src="image/csgo.jpg" alt="Counterstrike: Global Offence"> <br>
-           Rainbow Six Siege <img src="image/siege.jpg" alt="Rainbow Six Siege"> <br>
+           Rainbow Six Siege <br>
+           <img src="image/siege.jpg" alt="Rainbow Six Siege"> <br>
+        </p>
+    </div>
+
+    
            <?php
            mysqli_select_db($conn, 'join_up');
            $sql = "SELECT * FROM forum";
