5e18c2f5b2dae4293d2161de8139f7f577240d27
Finley <1040237@mborijnland.nl>
2019-05-22 10:52:46 +0200

    small changes

--- include/header.php
+++ include/header.php
@@ -3,8 +3,6 @@
         <ul>
             <div id="Homebtn"><li><a href="index.php">Home</a></li></div>
             <div id="Assortimentbtn"><li><a href="post.php">Een post maken</a></li></div>
-            <div id="Loginbtn"><li><a href="login.php">Login</a></li></div>
-            <div id="Registerbtn"><li><a href="registeren.php">registeren</a></li><div>
         </ul>
     </nav>
 </header>
