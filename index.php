f9f1f23ccad532fad3fc44ae5a724493a8887969
Finley <1040237@mborijnland.nl>
2019-05-23 10:40:15 +0200

    asdfsadf
    daniel moet presenteren

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ include("include/config.php");
           <p> Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.
         </p>
         </div>
-        <div class="col-sm w-100">
+        <div class="col-sm">
         <h2>Populaire games:</h2>
         <h3> League of Legends </h3> 
         <img class="game-img" src="image/lol.jpg" width="350px" height="200px" alt="League of Legends">
